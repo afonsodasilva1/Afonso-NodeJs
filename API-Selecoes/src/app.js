e8040318a112e15c60e6d1da343752751c9d1c66
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-01 12:02:11 +0100

    AS
    Mexendo com a rota Show Selecao

--- API-Selecoes/src/app.js
+++ API-Selecoes/src/app.js
@@ -1,14 +1,18 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 const selecoes = [
     {id:1, selecao: 'Angola', grupo:'A'},
     {id:2, selecao: 'Gabão', grupo:'A'},
     {id:3, selecao: 'França', grupo:'A'},
-    {id:4, selecao: 'Argentina', grupo:'A'},
-
+    {id:4, selecao: 'Argentina', grupo:'A'}
 ] 
+
+function getSelecaoById(id){
+    return selecoes.filter(selecao => selecao.id = id)
+}
 
 //Criando a rota padrão
 app.get('/', (req, res) => {
@@ -19,9 +23,15 @@ app.get('/showSelecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
 
+app.get('/showSelecao/:id', (req, res) =>{
+    res.status(200).json(getSelecaoById(req.params.id))
+})
+
 app.post('/createSelecao', (req, res) =>{
     selecoes.push(req.body)
-    res.send('Seleçao cadastrada com sucesso!')
+    res.status(201).send('Seleçao cadastrada com sucesso!')
 })
+
+
 
 export default app
