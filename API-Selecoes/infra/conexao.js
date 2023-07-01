e8b9dd8022a3266aad4bde132a5698ae5d5c15f9
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-01 13:24:52 +0100

    Update conexao.js

--- API-Selecoes/infra/conexao.js
+++ API-Selecoes/infra/conexao.js
@@ -8,4 +8,10 @@ dvdbvjdbvj hdb jd
 dvnkjdbvjdbv djbv ds
 
 
-Eu amo minha angola Terrra minha mãe EncodedVideoChunk,,  por ela eu vou luar
+Eu amo minha angola Terrra minha mãe EncodedVideoChunk,,  por ela eu vou ByteLengthQueuingStrategy
+
+Evânia
+Avelina
+Carla 
+Teresa
+Others
