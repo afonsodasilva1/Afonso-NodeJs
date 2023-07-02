7f166166ce6c200045b34c374e6b5eeebadc117b
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-02 13:30:43 +0100

    Update server.js

--- API-Selecoes/server.js
+++ API-Selecoes/server.js
@@ -1,15 +1,6 @@
 import app from "./src/app.js"
-import conection from "./infra/conexao.js"
 
 const PORT = 3000
-
-conection.connect((error) =>{
-    if(error){
-        console.log(error)
-    }else{
-        console.log('Conexão efectuada com sucesso!')
-            }
-})
 
 app.listen(PORT, () =>{
     console.log(`Servidor rodando no enderenço:http://localhost:${PORT}`)
