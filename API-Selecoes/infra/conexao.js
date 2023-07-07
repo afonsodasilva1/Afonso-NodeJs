fb788154c1df708c9f7002a5272a5ea7765ccdc4
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-07 23:22:21 +0100

    CONEXÃO

--- API-Selecoes/infra/conexao.js
+++ API-Selecoes/infra/conexao.js
@@ -7,7 +7,7 @@ const conection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    password: '',
     database: process.env.DATABASE
 })
 
