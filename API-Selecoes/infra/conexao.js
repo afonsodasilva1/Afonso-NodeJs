128592b629f6f94d4032227368ae10830029a961
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-07 22:43:38 +0100

    Arquivo env

--- API-Selecoes/infra/conexao.js
+++ API-Selecoes/infra/conexao.js
@@ -1,11 +1,13 @@
 import mysql from 'mysql'
 
+const dot = require('dotenv').config()
+
 const conection = mysql.createConnection({
-    host:'localhost',
-    port:'3306',
-    user:'root',
-    password:'',
-    database:'caf'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE
 })
 
 conection.connect()
--- API-Selecoes/server.js
+++ API-Selecoes/server.js
@@ -1,4 +1,5 @@
 import app from "./src/app.js"
+const dot = require('dotenv').config()
 
 const PORT = 3003
 
