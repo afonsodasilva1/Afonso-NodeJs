eb0a2c24069be93a67b9c23539d297f808a35318
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-07 23:29:22 +0100

    Update conexao.js

--- API-Selecoes/infra/conexao.js
+++ API-Selecoes/infra/conexao.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const conection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
     password: '',
-    database: process.env.DATABASE
+    database: process.en
 })
 
 conection.connect()
