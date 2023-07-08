5389b9c4b5d89085d172493e0c9a81ac784dcae5
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-08 09:50:47 +0100

    Update script.js

--- promise/script.js
+++ promise/script.js
@@ -1,3 +1,9 @@
 const myPromise = new Promise((resolve, reject) =>{
-    
+    const name = "Afonso"
+
+    if(nome === 'Afonso'){
+        console.log(resolve)
+    }else{
+        console.log(reject)
+    }
 })
