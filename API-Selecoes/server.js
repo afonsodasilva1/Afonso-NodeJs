f636c6f07fe82244916c85a8c67ed2b69f6e1345
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-08-15 19:05:33 +0100

    Fim-Bug
    Superei o bug do MYSQL

--- API-Selecoes/server.js
+++ API-Selecoes/server.js
@@ -2,7 +2,7 @@ import app from "./src/app.js"
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.DB_PORT
+const PORT = process.env.PORT
 
 app.listen(PORT, () =>{
     console.log(`Servidor rodando no enderenço:http://localhost:${PORT}/showSelecoes`)
