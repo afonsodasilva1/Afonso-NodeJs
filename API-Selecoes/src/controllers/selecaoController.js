c1bba6854d927cee06e5e7bd1df169b6154b8601
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-08-15 20:15:31 +0100

    Update selecaoController.js

--- API-Selecoes/src/controllers/selecaoController.js
+++ API-Selecoes/src/controllers/selecaoController.js
@@ -7,7 +7,7 @@ class SelecaoController{
             if(erro){
                 res.status(404).json({'erro': erro})
             }else{
-                res.status(200).json(result)
+                res.status(200).json({'resultado': result})
             }
         })
     }
@@ -19,9 +19,10 @@ class SelecaoController{
             if(erro)
                 res.status(400).json({'erro': erro})
             else
-                res.status(200).json(result)
+                res.status(200).json({'resultado':result})
         })
     } 
+    
     store(req, res){
         const selecao = req.body
         const sqlComand = 'INSERT INTO selecao SET ?;'
@@ -34,7 +35,28 @@ class SelecaoController{
     }
 
     update(req, res){
-        
+        const selecao   = req.body
+        const id        = req.params.id
+        const sqlComand = 'UPDATE selecoes SET ? WHERE id = ?'
+
+        conection.query(sqlComand, [selecao, id], (erro, result) =>{
+            if(erro)
+                res.status(400).json({'erro': erro})
+            else
+                res.status(200).json({'resultado': result})
+        })
+    }
+
+    delete(req, res){
+        const id = req.params.id
+        const sqlComand = 'DELETE FROM selecao WHERE id = ?'
+
+        conection.query(sqlComand, id, (erro, result) =>{
+            if(erro)
+                res.status(400).json({'erro': erro})
+            else
+                res.status(200).json({'resultado': result})
+        })
     }
 }
 
