2b52418bbe2beb6ff16009a1550de21890b2b3d0
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-08-20 15:17:50 +0100

    Update selecaoController.js

--- API-Selecoes/src/controllers/selecaoController.js
+++ API-Selecoes/src/controllers/selecaoController.js
@@ -1,4 +1,3 @@
-import conection from '../models/conexao.js'
 import SelecaoRepository from '../Repositories/SelecaoRepository.js'
 
 class SelecaoController{
@@ -13,15 +12,8 @@ class SelecaoController{
         res.json(row)
     } 
     
-    store(req, res){
-        const selecao = req.body
-        const sqlComand = 'INSERT INTO selecao SET ?;'
-        conection.query(sqlComand, selecao, (erro, result) =>{
-            if(erro)
-                res.status(404).json({'erro':erro})
-            else
-                res.status(201).json({'result': result})
-        })
+    async store(req, res){
+       const row = await SelecaoRepository.create
     }
 
     async update(req, res){
@@ -29,16 +21,9 @@ class SelecaoController{
        res.json(row)
     }
 
-    delete(req, res){
-        const id = req.params.id
-        const sqlComand = 'DELETE FROM selecao WHERE id = ?'
-
-        conection.query(sqlComand, id, (erro, result) =>{
-            if(erro)
-                res.status(400).json({'erro': erro})
-            else
-                res.status(200).json({'resultado': result})
-        })
+    async delete(req, res){
+        const row = await SelecaoRepository.delete(req.params.id)
+        res.json(row)
     }
 }
 
